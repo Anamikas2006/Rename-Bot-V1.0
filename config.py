5f8ae8fbe6ef0e24918e41de69f99e3d01e86cb9
Anamikas2006 <126434336+Anamikas2006@users.noreply.github.com>
2023-03-08 17:33:09 +0530

    Update config.py
    Signed-off-by: Anamikas2006 <126434336+Anamikas2006@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "26551994")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "80bb600d382bb46ce394c2808e1ef2ac")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6237341566:AAENXTuZecPPduK5ZvJIiKIJTYUeMCzSH7U") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "botosteeve") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Renamebotv1:Renamebotv1@cluster0.nz721xb.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
+START_PIC = os.environ.get("START_PIC", "https://images.app.goo.gl/623TXcmCEsGtvuPL7")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1591539219').split()]
 
 PORT = os.environ.get("PORT", "8080")
